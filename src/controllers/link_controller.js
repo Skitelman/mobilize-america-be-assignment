6aa1f273a72d9257186f664565703aa8a5c164c1
Sam Ghitelman <sghitelman@gmail.com>
2019-07-30 18:54:44 -0400

    Allow users to make a custom URL for the short link

--- src/controllers/link_controller.js
+++ src/controllers/link_controller.js
@@ -10,11 +10,21 @@ linkController.post(
   asyncHandler(async (req, res) => {
     const { db } = res.locals;
     const destinationUrl = idx(req, _ => _.body.destinationUrl);
+    const customUrl = idx(req, _ => _.body.customUrl);
 
+    // Return an error message if the user fails to provide a destinationUrl
     if (!destinationUrl) {
       return res.status(400).json({
         error: {
           message: 'You must provide a destinationUrl'
+        }
+      })
+    }
+
+    if(customUrl && customUrl.length > 20) {
+      return res.status(400).json({
+        error: {
+          message: 'Your custom link must be 20 characters or fewer'
         }
       })
     }
@@ -35,28 +45,33 @@ linkController.post(
       })
     }
 
-    // Find a random unusued string to use for the shortUrl
-    let randAddress = '';
-    while (randAddress.length === 0) {
-      const randString = randomstring.generate({
-        length: 7,
-        charset: 'alphanumeric',
-      });
-      const links = await db.models.Link.findAll({
-        where: {
-          shortUrl: randString,
+    
+    let shortAddress = '';
+    if (customUrl) {
+      shortAddress = customUrl;
+    } else {
+      // Find a random unusued string to use for the shortUrl
+      while (shortAddress.length === 0) {
+        const randString = randomstring.generate({
+          length: 7,
+          charset: 'alphanumeric',
+        });
+        const links = await db.models.Link.findAll({
+          where: {
+            shortUrl: randString,
+          }
+        });
+        if (links.length === 0) {
+          shortAddress = randString;
         }
-      });
-      if (links.length === 0) {
-        randAddress = randString;
       }
     }
 
     const link = await db.models.Link.create({
       destinationUrl,
-      shortUrl: randAddress,
+      shortUrl: shortAddress,
     }) 
-    const shortUrl = `http://localhost:3000/${randAddress}`;
+    const shortUrl = `http://localhost:3000/${shortAddress}`;
     console.log('New link ID: ', link.id);
     return res.status(200).json({
       data: {
