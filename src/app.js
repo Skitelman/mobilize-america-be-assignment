99ed539b1c30a0ef37a6e76ea6942367ca894e74
Sam Ghitelman <sghitelman@gmail.com>
2019-07-30 17:28:07 -0400

    Add Sequelize and initial migration

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import cors from 'cors';
 import idx from 'idx';
+import asyncHandler from 'express-async-handler';
+
+import dbClient from './services/dbClient';
 
 import redirectController from './controllers/redirect_controller';
 import linkController from './controllers/link_controller';
@@ -11,13 +14,22 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Make db client in res object
+app.use(async (req, res, next) => {
+  res.locals = {
+    db: dbClient,
+  };
+  return next();
+})
+
 app.use(redirectController);
 app.use('/link', linkController);
 app.use('/stats', statsController);
 
-app.get('/', (req, res) => {
+app.get('/', asyncHandler(async (req, res) => {
+  await res.locals.db.authenticate(); 
   res.status(200).json({ data: 'The Shortest URL of All!' });
-});
+}));
 
 app.listen(3000, () => {
   console.log('Sam\'s link shortener is open for business');
---
+++ src/services/dbClient.js
@@ -0,0 +1,17 @@
+import Sequelize from 'sequelize';
+import * as databaseConfig from '../../config/database.json';
+import Link from '../models/Link';
+
+const env = process.env.NODE_ENV || 'development';
+const config = databaseConfig[env];
+
+const options = {
+  host: config.host,
+  dialect: config.dialect,
+  logging: console.log,
+}
+
+const dbClient = new Sequelize(config.database, config.username, config.password, options);
+
+
+export default dbClient;
